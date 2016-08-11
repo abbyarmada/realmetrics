91b9a06c3cea325b38557f0b9cb14c34744de813
Dominic Goulet <dominic.goulet@gmail.com>
2016-08-11 16:34:47 -0400

    codeclimate file

--- app/models/reports/revenues_report.rb
+++ app/models/reports/revenues_report.rb
@@ -6,13 +6,8 @@ class RevenuesReport < ReportBase
 
   def render
     @data = []
+
     @kpis = {
-      ytd: {
-        amount: 0,
-        goal: 0,
-        completion: 0,
-        growth: 0
-      },
       mtd: {
         amount: 0,
         goal: 0,
@@ -21,67 +16,43 @@ class RevenuesReport < ReportBase
       }
     }
 
-    current_year = nil
+    last_year = nil
 
     results.each do |res|
-      current_month = res['month'].to_i
+      year = res['year'].to_i
+      month = res['month'].to_i
 
-      if current_year != res['year'].to_i
-        current_year = res['year'].to_i
+      if last_year != year
+        last_year = year
 
         @data << {
-          year: res['year'].to_i,
-          data: []
+          year: year,
+          data: Array.new(12)
         }
-
-        12.times do |idx|
-          @data[-1][:data] << {
-            month: idx + 1,
-            amount: 0,
-            goal: 0,
-            completion: 0,
-            growth: 0
-          }
-        end
       end
 
-      current_amount = res['amount'].to_i * 1.0
-      current_goal = res['goal'].to_i * 1.0
-      last_amount = @data[-1][:data][current_month - 2][:amount]
-
-      @data[-1][:data][current_month - 1][:amount] = current_amount
-      @data[-1][:data][current_month - 1][:goal] = current_goal
-      @data[-1][:data][current_month - 1][:completion] = (current_amount / current_goal * 100.0).round(0) if current_goal > 0
-      @data[-1][:data][current_month - 1][:growth] = (current_amount / last_amount * 100.0).round(0) - 100 if last_amount.present? && last_amount > 0 && current_amount > 0
-
-      if current_month == Time.zone.today.month && current_year == Time.zone.today.year
-        @kpis[:mtd][:amount] = @data[-1][:data][current_month - 1][:amount]
-        @kpis[:mtd][:goal] = @data[-1][:data][current_month - 1][:goal]
-        @kpis[:mtd][:completion] = @data[-1][:data][current_month - 1][:completion]
-        @kpis[:mtd][:growth] = @data[-1][:data][current_month - 1][:growth]
-      end
-
-      if @data[-1][:data][current_month - 1][:amount] > 0
-        @kpis[:ytd][:amount] += @data[-1][:data][current_month - 1][:amount]
-        @kpis[:ytd][:goal] += @data[-1][:data][current_month - 1][:goal]
-      end
+      @data[-1][:data][month - 1] = {
+        month: res['month'].to_i,
+        amount: res['amount'].to_i,
+        goal: res['goal'].to_i,
+        completion: res['completion'].to_i,
+        growth: res['growth'].to_i
+      }
     end
 
-    if @kpis[:ytd][:goal] > 0
-      @kpis[:ytd][:completion] = (@kpis[:ytd][:amount] / @kpis[:ytd][:goal] * 100.0).round(0)
-    end
+    @kpis[:ytd] = {
+      amount: @data.sum { |x| x[:data].sum { |y| y[:amount] } },
+      goal: @data.sum { |x| x[:data].sum { |y| y[:goal] } },
+      completion: @data.sum { |x| x[:data].sum { |y| y[:completion] } },
+      growth: @data.sum { |x| x[:data].sum { |y| y[:growth] } }
+    }
 
-    # last non null ON first non null
-    first_non_null = 0
-    last_non_null = 0
-
-    @data[-1][:data].each do |row|
-      first_non_null = row[:amount] if row[:amount] > 0 && first_non_null == 0
-      last_non_null = row[:amount] if row[:amount] > 0
-    end
-    first_non_null = 1 if first_non_null == 0
-
-    @kpis[:ytd][:growth] = (last_non_null / first_non_null * 100.0).round(0) - 100
+    @kpis[:mtd] = {
+      amount: @data.sum { |x| x[:data].sum { |y| y[:month] < Date.today.month ? y[:amount] : 0 } },
+      goal: @data.sum { |x| x[:data].sum { |y| y[:month] < Date.today.month ? y[:goal] : 0 } },
+      completion: @data.sum { |x| x[:data].sum { |y| y[:month] < Date.today.month ? y[:completion] : 0 } },
+      growth: @data.sum { |x| x[:data].sum { |y| y[:month] < Date.today.month ? y[:growth] : 0 } }
+    }
   end
 
   def results
@@ -90,6 +61,8 @@ class RevenuesReport < ReportBase
            , B.month
            , SUM(stripe_transactions.gross_amount) AS amount
            , goals.value AS goal
+           , SUM(stripe_transactions.gross_amount) * 100.0 / goals.value AS completion
+           , 0 AS growth
         FROM (SELECT generate_series(
                        CAST(EXTRACT(YEAR FROM :from_date::DATE) AS INTEGER),
                        CAST(EXTRACT(YEAR FROM :to_date::DATE) AS INTEGER)
